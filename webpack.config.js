138102516cb84fa859815f92fb196729f7982cba
XPC2659489932 <2659489932@qq.com>
2019-06-10 12:30:26 +0800

    initial

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path              = require('path');
+const webpack           = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: './src/app.jsx',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		publicPath:'/dist/',
+		filename: 'js/app.js'
+  },
+	module: {
+		rules: [
+		    //react(jsx)文件语法的处理 
+		    {
+			    test: /\.jsx$/,
+			    exclude: /(node_modules)/,
+			    use: {
+				    loader: 'babel-loader',
+				    options: {
+				        presets: ['env','react']
+			        }
+			    }
+		    },
+		    //css文件语法的处理 
+		    {
+		        test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+				    fallback: "style-loader",
+				    use: "css-loader"
+				})
+	        },
+	        //sass文件语法的处理
+	        {
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'sass-loader']
+				})
+			},
+			//图片的配置 
+			{
+				test: /\.(png|jpg|gif)$/,
+				use: [
+						{
+							loader: 'url-loader',
+							options: {
+							  limit: 8192,
+							  name: 'resource/[name].[ext]'
+						}
+					}
+				]
+			},
+			//字体图标的配置
+			{
+				test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
+				use: [
+						{
+							loader: 'url-loader',
+							options: {
+							  limit: 8192,
+							  name: 'resource/[name].[ext]'
+						}
+					}
+				]
+			}
+		]
+	},
+   plugins: [
+        //处理html文件
+        new HtmlWebpackPlugin({
+        	template: './src/index.html'
+        }),
+        //独立css文件
+        new ExtractTextPlugin("css/[name].css"),
+        //提出公共模块 
+        new webpack.optimize.CommonsChunkPlugin({
+        	name : 'common',
+        	filename : 'js/base.js'
+        })
+   ],
+   devServer: {
+        port:3200
+   }
+};
