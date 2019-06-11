66a3e4720f14509b3aea116eacd08afa8fd20a55
XPC2659489932 <2659489932@qq.com>
2019-06-11 23:16:12 +0800

    initial

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router ,Route,Link} from 'react-router-dom';
 
-import 'font-awesome/css/font-awesome.min.css';
-import './index.css';
-import './index.scss';
-
-/*class HelloMessage extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello {this.props.name}
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(
-  <HelloMessage name="Taylor" />,
-  document.getElementById('app')
-);*/
-
-ReactDOM.render(
-	<div>
-    <h1>Hello, world!</h1>,
-    </div>,
-    document.getElementById('app')
-)
